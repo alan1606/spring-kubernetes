311a869bba17cfb84b189dea977c31880a72e027
alan1606 <62415210+alan1606@users.noreply.github.com>
2024-02-06 20:02:41 -0600

    Service e implementación

---
+++ msvc-usuarios/src/main/java/lat/alanaguirre/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -0,0 +1,42 @@
+package lat.alanaguirre.msvc.usuarios.services;
+
+import lat.alanaguirre.msvc.usuarios.models.entity.Usuario;
+import lat.alanaguirre.msvc.usuarios.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements  UsuarioService{
+    private UsuarioRepository repository;
+
+    public UsuarioServiceImpl(UsuarioRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> findAll() {
+        return (List<Usuario>) repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Usuario> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Usuario save(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
